095d3b16c2547d1546bb50a818ab559776e860da
Dávid János Fekete <janosdavidfekete@gmail.com>
2023-12-24 14:32:07 +0100

    0.1 verzió - segítség kérési opció

--- main.py
+++ main.py
@@ -19,32 +19,43 @@ class App:
     
     def jatek(self):
         self.valtozo = ''
-        
+        print('Ha szabad a gazda, akkor írd be, hogy: szabad')
+        print('Ha nem tudsz egy szót, írd be, hogy: elso vagy masodik vagy harmadik')
         #különböző esetek
         while self.valtozo != self.szo:
             try:
                 self.valtozo = input('Add meg a szavadat: ')
-                if self.valtozo[0] == self.szo[0] and self.valtozo[1] != self.szo[1] and self.valtozo[2] != self.szo[2]:
-                    print(f'{self.szo[0]} * *')
-                elif self.valtozo[0] != self.szo[0] and self.valtozo[1] == self.szo[1] and self.valtozo[2] != self.szo[2]:
-                    print(f'* {self.szo[1]} *')
-                elif self.valtozo[0] != self.szo[0] and self.valtozo[1] != self.szo[1] and self.valtozo[2] == self.szo[2]:
-                    print(f'* * {self.szo[2]}')
-                elif self.valtozo[0] == self.szo[0] and self.valtozo[1] == self.szo[1] and self.valtozo[2] != self.szo[2]:
-                    print(f'{self.szo[0]} {self.szo[1]} *')
-                elif self.valtozo[0] != self.szo[0] and self.valtozo[1] == self.szo[1] and self.valtozo[2] == self.szo[2]:
-                    print(f'* {self.szo[1]} {self.szo[2]}')
-                elif self.valtozo[0] == self.szo[0] and self.valtozo[1] != self.szo[1] and self.valtozo[2] == self.szo[2]:
-                    print(f'{self.szo[0]} * {self.szo[2]}')
-                
-                #ha szabad a gazda
-                elif self.valtozo == 'szabad':
-                    print(self.szo)
-                    break
-                    
-                    #ha kitalálta
-                elif self.valtozo == self.szo:
-                    print('Gratulálok, eltaláltad!')
+                if len(self.valtozo) ==3:
+                    if self.valtozo[0] == self.szo[0] and self.valtozo[1] != self.szo[1] and self.valtozo[2] != self.szo[2]:
+                        print(f'{self.szo[0]} * *')
+                    elif self.valtozo[0] != self.szo[0] and self.valtozo[1] == self.szo[1] and self.valtozo[2] != self.szo[2]:
+                        print(f'* {self.szo[1]} *')
+                    elif self.valtozo[0] != self.szo[0] and self.valtozo[1] != self.szo[1] and self.valtozo[2] == self.szo[2]:
+                        print(f'* * {self.szo[2]}')
+                    elif self.valtozo[0] == self.szo[0] and self.valtozo[1] == self.szo[1] and self.valtozo[2] != self.szo[2]:
+                        print(f'{self.szo[0]} {self.szo[1]} *')
+                    elif self.valtozo[0] != self.szo[0] and self.valtozo[1] == self.szo[1] and self.valtozo[2] == self.szo[2]:
+                        print(f'* {self.szo[1]} {self.szo[2]}')
+                    elif self.valtozo[0] == self.szo[0] and self.valtozo[1] != self.szo[1] and self.valtozo[2] == self.szo[2]:
+                        print(f'{self.szo[0]} * {self.szo[2]}') 
+                        
+                    #ha eltalálta    
+                    elif self.valtozo == self.szo:
+                        print('Gratulálok, eltaláltad!')
+                      
+                else:
+                    #ha szabad a gazda
+                    if self.valtozo == 'szabad':
+                        print(self.szo)
+                        break
+                        
+                    #ha segítséget kért
+                    elif self.valtozo == 'elso':
+                        print(self.szo[0])
+                    elif self.valtozo == 'masodik':
+                        print(self.szo[1])
+                    elif self.valtozo == 'harmadik':
+                        print(self.szo[2])
                     
             except IndexError:
                 print('Három betűs szavakat adj meg!')
