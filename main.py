189cf7a9fde4b1c7e13f5b49ec0b095c435b43d7
Dávid János Fekete <janosdavidfekete@gmail.com>
2023-12-24 14:00:21 +0100

    0.0 verzió - lehet vele játszani

--- main.py
+++ main.py
@@ -17,10 +17,41 @@ class App:
     def szo_valasztas(self):
         self.szo = random.choice(self.harom_betus)
     
-    def kitalalas(self):
-        print(len(self.szo))
+    def jatek(self):
+        self.valtozo = ''
+        
+        #különböző esetek
+        while self.valtozo != self.szo:
+            try:
+                self.valtozo = input('Add meg a szavadat: ')
+                if self.valtozo[0] == self.szo[0] and self.valtozo[1] != self.szo[1] and self.valtozo[2] != self.szo[2]:
+                    print(f'{self.szo[0]} * *')
+                elif self.valtozo[0] != self.szo[0] and self.valtozo[1] == self.szo[1] and self.valtozo[2] != self.szo[2]:
+                    print(f'* {self.szo[1]} *')
+                elif self.valtozo[0] != self.szo[0] and self.valtozo[1] != self.szo[1] and self.valtozo[2] == self.szo[2]:
+                    print(f'* * {self.szo[2]}')
+                elif self.valtozo[0] == self.szo[0] and self.valtozo[1] == self.szo[1] and self.valtozo[2] != self.szo[2]:
+                    print(f'{self.szo[0]} {self.szo[1]} *')
+                elif self.valtozo[0] != self.szo[0] and self.valtozo[1] == self.szo[1] and self.valtozo[2] == self.szo[2]:
+                    print(f'* {self.szo[1]} {self.szo[2]}')
+                elif self.valtozo[0] == self.szo[0] and self.valtozo[1] != self.szo[1] and self.valtozo[2] == self.szo[2]:
+                    print(f'{self.szo[0]} * {self.szo[2]}')
+                
+                #ha szabad a gazda
+                elif self.valtozo == 'szabad':
+                    print(self.szo)
+                    break
+                    
+                    #ha kitalálta
+                elif self.valtozo == self.szo:
+                    print('Gratulálok, eltaláltad!')
+                    
+            except IndexError:
+                print('Három betűs szavakat adj meg!')
+    #def jatek vége
             
+                     
 app = App()
 app.fajl_beolvas()
 app.szo_valasztas()
-app.kitalalas()
+app.jatek()
